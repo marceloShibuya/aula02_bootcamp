5d8f49521591a6d06765b56676f818fcdef47832
Marcelo Shibuya <marcelo.hshibuya@gmail.com>
2024-06-24 21:02:31 -0300

    adicionando exercicio 12

--- exercicios.py
+++ exercicios.py
@@ -79,11 +79,15 @@
 # #### Strings (`str`)
 
 # 11. Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
-minusculo = str(input("Digite qualquer palavra em letra minuscula: "))
-maiusculo = minusculo.upper()
-print(maiusculo)
+# minusculo = str(input("Digite qualquer palavra em letra minuscula: "))
+# maiusculo = minusculo.upper()
+# print(maiusculo)
 
 # 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
+maiusculo = str(input("Digite qualquer palavra em letra maiuscula: "))
+minusculo = maiusculo.lower()
+print(minusculo)
+
 # 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
 # 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.da
 # data_usuario = input("Insira uma data no formato DD/MM/AAAA: ")
